45a2968447fd0a790c607234542d017b369eb3f8
Nattapong Anuwong <nattapong.a@techcombine.co>
2025-09-01 13:35:41 +0700

    fix: build

--- next.config.ts
+++ next.config.ts
@@ -9,16 +9,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    // Ignore node-specific modules when bundling for the browser
-    // https://webpack.js.org/configuration/resolve/#resolvealias
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      sharp$: false,
-      "onnxruntime-node$": false,
-    };
-    return config;
-  },
+  serverExternalPackages: ["sharp", "onnxruntime-node"],
 };
 
 export default nextConfig;
