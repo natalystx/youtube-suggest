25203f53aaefa3e8066e9c7c522540dc0f071b52
Nattapong Anuwong <nattapong.a@techcombine.co>
2025-09-01 13:02:35 +0700

    chore: ignore onnx

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,16 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    // Ignore node-specific modules when bundling for the browser
+    // https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      sharp$: false,
+      "onnxruntime-node$": false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
